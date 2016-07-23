086d8e60bcc412899550d771798031a9d414d540
Kevin Flynn <666c796e6e@users.noreply.github.com>
2016-07-23 16:06:14 +0200

    Created Singlefunctions and DigitCommandParsers

--- Another_VFD_GateFinder.ino
+++ Another_VFD_GateFinder.ino
@@ -48,16 +48,14 @@ void SevenSegments::begin (
      _masks[0x06] = top | topLeft | middle | bottomRight | bottom | bottomLeft;
      _masks[0x07] = top | topRight | bottomRight;
      _masks[0x08] = top | topRight |  middle | bottomRight | bottom | bottomLeft | topLeft;
-     _masks[0x09] = top | topRight |  middle | bottom | bottomLeft | topLeft; 
+     _masks[0x09] = top | topRight |  middle | bottom | bottomRight | topLeft; 
 };
 
 unsigned long SevenSegments::getMaskForDigit(byte digit) {
+  Serial.print("Mask for Digit: ");
+  Serial.println(digit);
   if (digit > 10) return 0L;
   unsigned long ret = _masks[digit];
-  Serial.print("digit -- mask: ");
-  Serial.print(digit);
-  Serial.print(" -- ");
-  Serial.println(ret);
   return ret;
 };
 
@@ -67,31 +65,41 @@ unsigned long SevenSegments::getMaskForClearing(){
 
 class SevenSegmentEncoder {
   private:
-    SevenSegments* _mySegments;
+    SevenSegments* _mySegments[5];
+    unsigned long* _myBins[5];
+
     byte _numSegments;
     byte _radix;
   
   public:
-    void begin(SevenSegments* mySegments, byte numSegments, byte radix);
-    void encode(int number, unsigned long* buffer);
+    void begin(byte radix);
+    void addSegment(SevenSegments* segment, unsigned long* bin);
+    void encode(int number);
 };
 
-void SevenSegmentEncoder::begin(SevenSegments* mySegments, byte numSegments, byte radix) {
-  _mySegments = mySegments;
-  _numSegments = numSegments;
+void SevenSegmentEncoder::begin(byte radix) {
   _radix = radix;
 };
 
-void SevenSegmentEncoder::encode(int number, unsigned long* buffer) {
+void SevenSegmentEncoder::addSegment(SevenSegments* segment, unsigned long* bin){
+  _mySegments[_numSegments]=segment;
+  _myBins[_numSegments]=bin;
+  _numSegments++;
+}
+
+void SevenSegmentEncoder::encode(int number) {
+  Serial.print("Trying to encode: ");
+  Serial.println(number);
   if (number < 0 ) {
     number = -number;
     //TODO: Handle numbersign.
   }
   
   for (int i=_numSegments-1; i>=0; i--) {
+    Serial.println(i);
     byte digit = number % _radix;
     number = number / _radix;
-    buffer[i] = _mySegments[i].getMaskForDigit(digit);
+    (*_myBins[i]) |= _mySegments[i]->getMaskForDigit(digit);
   }
   //TODO: Handle overflow
 };
@@ -163,97 +171,187 @@ void AnotherMultiplexer::cycle() {
   if (_currentBin >= _numBins) _currentBin = 0;
 }
 
+class SingleFunction {
+  private:
+    unsigned long* _bin;
+    String _memonic;
+    unsigned long _mask;
+    
+  public:
+    void begin(unsigned long*, unsigned long mask, String memonic);
+    boolean tryObey(String command);
+    String getMemonic();
+};
 
+void SingleFunction::begin(unsigned long* targetBin, unsigned long mask, String memonic) {
+  _bin=targetBin;
+  _mask=mask;
+  _memonic=memonic;
+};
+
+boolean SingleFunction::tryObey(String command){
+  if (_memonic.equals(command.substring(0,_memonic.length()))) {
+    String wantsOnOrOff = command.substring(_memonic.length()+1,_memonic.length()+2);   
+    if (wantsOnOrOff.equals("1")) {
+      (*_bin)|=_mask;
+    } else {
+      (*_bin)&=~_mask;
+    }
+    return true;
+  }
+  return false;
+};
+
+String SingleFunction::getMemonic() {
+  return _memonic;
+}
+
+class DigitCommandParser {
+  private:
+    String _memonic;
+    SevenSegmentEncoder* _encoder;
+  
+  public:
+    void begin(String memonic, SevenSegmentEncoder* encoder);
+    boolean tryObey(String command);
+    String getMemonic();
+};
+
+void DigitCommandParser::begin(String memonic, SevenSegmentEncoder *encoder) {
+  _memonic = memonic;
+  _encoder = encoder;
+}
+
+boolean DigitCommandParser::tryObey(String command) {
+  if (command.substring(0,_memonic.length()).equals(_memonic)) {
+     Serial.print("Command: ");
+     Serial.println(command);
+     String numberPart = command.substring(_memonic.length()+1,command.length()-1);
+     Serial.print("Numberpart: ");
+     Serial.println(numberPart);
+     if (numberPart.equals("off")) {
+       
+     } else {
+       int number = strtoul(numberPart.c_str(),NULL,10);
+       Serial.println(number);
+       Serial.println((unsigned long)_encoder,HEX);
+       _encoder->encode(number);
+    }
+    return true;
+  }
+  return false;
+  
+};
+
+String DigitCommandParser::getMemonic() {
+  return _memonic;
+}
 
 class AnotherVFD {
   private:
     unsigned long _bins[4];
-    unsigned long _grids[4];
     
     //Several Grids
     SevenSegments _rightseg;
     SevenSegments _leftseg;
-    SevenSegments _segments[2];
-    SevenSegments _segments2[2];
-    SevenSegmentEncoder _encoder;
-    SevenSegmentEncoder _encoder2;
+    SevenSegmentEncoder _encoder[3];
+    DigitCommandParser _parsers[3];
    
-    
-    //Grid 0
-    
-    //Grid 1
-    
-    //Grid 2
-    
-    //Grid 3
+    //Gate 0
     unsigned long _dvd[6];
+    unsigned long _dvdcenter = 0x400;
+    unsigned long _dvdOffMask = 0x3F0;
     
-   void doSetDigit(int number, int binleft, int binright, int swapem);
+    SingleFunction _singlefunctions[10];
+    int _numSingleFunctions=0;
+    
+    //Gate 1
+    unsigned long v = 0x10;
+    unsigned long cd= 0x20;
+    unsigned long p = 0x40;
+    unsigned long commas = 0x80;
+    unsigned long speakerLeft = 0x200;
+    unsigned long speakerRight = 0x400;
+    unsigned long middleDash = 0x100;
+    
+    byte dvdAnimationCounter=0;
    
   public:
     AnotherVFD();
     unsigned long* getBins();
-   void setDigit(int digit, int number);
+    void stepDvdAnimation();
+    void dvdOff();
+    void tryObey(String command);
 };
 
 AnotherVFD::AnotherVFD()
 {
-  _grids[3] = 0x01;
-  _grids[2] = 0x02;
-  _grids[1] = 0x04;
-  _grids[0] = 0x08;
-  
-  for(int i=0;i<4;i++)
-    _bins[i]|=_grids[i];
+  _bins[0] = 0x01;
+  _bins[1] = 0x02;
+  _bins[2] = 0x04;
+  _bins[3] = 0x08;
 
   
-  _dvd[0] = 0x10;
-  _rightseg.begin(0x400, 0x100, 0x200, 0x80, 0x20, 0x40, 0x10, 0x00);
-  _leftseg.begin(0x40000, 0x10000, 0x20000, 0x8000, 0x2000, 0x4000, 0x1000, 0x00);
-  _segments[0]=_leftseg;
-  _segments[1]=_rightseg;
-  _encoder.begin(_segments,2,10);
-  _segments2[0]=_leftseg;
-  _segments2[1]=_leftseg;
-  _encoder2.begin(_segments2,2,10);
-}
+  _dvd[0]=0x10;
+  _dvd[1]=0x20;
+  _dvd[2]=0x40;
+  _dvd[3]=0x80;
+  _dvd[4]=0x100;
+  _dvd[5]=0x200;
+  
+  _leftseg.begin(0x10, 0x40, 0x20, 0x80, 0x200, 0x100, 0x400, 0x00);
+  _rightseg.begin(0x1000, 0x4000, 0x2000, 0x8000, 0x20000, 0x10000, 0x40000, 0x00);
+  
+  _encoder[0] = SevenSegmentEncoder();
+  _encoder[0].begin(10);
+  _encoder[0].addSegment(&_rightseg,_bins+0);
+  _encoder[0].addSegment(&_rightseg,_bins+1);
+  _parsers[0] = DigitCommandParser();
+  _parsers[0].begin("digit1",_encoder+0);
+  
+  _encoder[1] = SevenSegmentEncoder();
+  _encoder[1].begin(10);
+  _encoder[1].addSegment(&_leftseg, _bins+2);
+  _encoder[1].addSegment(&_rightseg,_bins+2);
+  _parsers[1] = DigitCommandParser();
+  _parsers[1].begin("digit2",_encoder+1);
+  
+  _encoder[2] = SevenSegmentEncoder();
+  _encoder[2].begin(10);
+  _encoder[2].addSegment(&_leftseg, _bins+3);
+  _encoder[2].addSegment(&_rightseg,_bins+3);
+  _parsers[2] = DigitCommandParser();
+  _parsers[2].begin("digit3",_encoder+2);
+  
+  
+  _singlefunctions[_numSingleFunctions]=SingleFunction();
+  _singlefunctions[_numSingleFunctions++].begin(_bins+1,0x10,"v");
+};
+
+void AnotherVFD::tryObey(String command) {
+  for(int i=0;i<_numSingleFunctions;i++) {
+    if (_singlefunctions[i].tryObey(command)) return;
+  }
+  for(int i=0;i<3;i++) {
+    if (_parsers[i].tryObey(command)) return;
+  }
+};
 
 unsigned long* AnotherVFD::getBins() {
   return _bins;
+};
+
+void AnotherVFD::stepDvdAnimation(){
+  _bins[0]|=_dvdOffMask;
+  _bins[0]&=~_dvd[dvdAnimationCounter++];
+  _bins[0]^=_dvdcenter;
+  if (dvdAnimationCounter>=6)
+    dvdAnimationCounter=0;
+};
+
+void AnotherVFD::dvdOff(){
+  _bins[3]&=~_dvdOffMask;
 }
-
-void AnotherVFD::setDigit(int digit, int number){
-   switch(digit){
-      case 1:
-        doSetDigit(number,0,0,0);
-        break;
-      case 2:
-        doSetDigit(number,1,1,0);
-        break;
-      case 3:
-        _bins[3]&=~_rightseg.getMaskForClearing();
-        _bins[2]&=~_rightseg.getMaskForClearing();
-        unsigned long buffer[2];
-        _encoder2.encode(number,buffer);
-        _bins[2]|=buffer[0];
-        _bins[3]|=buffer[1];
-        break;
-   }
-}
-
-void AnotherVFD::doSetDigit(int number, int binleft, int binright, int swapem){
-
-  _bins[binright]&=~_rightseg.getMaskForClearing();
-  _bins[binleft]&=~_leftseg.getMaskForClearing();
-  
-  unsigned long buffer[2];
-  _encoder.encode(number, buffer);
-  
-  _bins[binleft]|=buffer[0^swapem];
-  _bins[binright]|=buffer[1^swapem];
-  
-}
-
 
 Shifter shifter(2,3,4,0);
 AnotherVFD anotherVFD;
@@ -263,7 +361,7 @@ AnotherMultiplexer plexi(&shifter, anotherVFD.getBins(), 4);
 void setup() {
   
   Serial.begin(9600);
-  Timer1.initialize(1000);
+  Timer1.initialize(2000);
   Timer1.attachInterrupt(timerRoutine);
   shifter.begin();
   pinMode(13,OUTPUT);
@@ -273,7 +371,8 @@ boolean pinState13=false;
 int counter=0;
 
 void timerRoutine(){
-  if (counter > 1000) {
+  if (counter > 50) {
+    anotherVFD.stepDvdAnimation();
     digitalWrite(13,pinState13);
     pinState13=~pinState13;
     counter=0;
@@ -288,27 +387,7 @@ void loop(){
    if (Serial.available()>0) {
        String stringcoming = Serial.readString();
 
-       if (stringcoming.substring(0,3).equals("d1=")) {
-         Serial.print("Setting digit one =");
-         String numberPart = stringcoming.substring(3);
-         int number = strtoul(numberPart.c_str(),NULL,10);
-         Serial.println(number);
-         anotherVFD.setDigit(1,number);
-       }
-        if (stringcoming.substring(0,3).equals("d2=")) {
-         Serial.print("Setting digit one =");
-         String numberPart = stringcoming.substring(3);
-         int number = strtoul(numberPart.c_str(),NULL,10);
-         Serial.println(number);
-         anotherVFD.setDigit(2,number);
-       }
-       if (stringcoming.substring(0,3).equals("d3=")) {
-         Serial.print("Setting digit one =");
-         String numberPart = stringcoming.substring(3);
-         int number = strtoul(numberPart.c_str(),NULL,10);
-         Serial.println(number);
-         anotherVFD.setDigit(3,number);
-       }
+       anotherVFD.tryObey(stringcoming);
 
     }
 
