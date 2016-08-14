a625f715d0d91cf073413debc95ed5d0b2a39ed2
Kevin Flynn <666c796e6e@users.noreply.github.com>
2016-08-14 23:45:42 +0200

    Minor improvements

--- examples/Vfd_Controller/Vfd_Controller.ino
+++ examples/Vfd_Controller/Vfd_Controller.ino
@@ -11,8 +11,13 @@
 #include <MemoryFree.h>
 #include <VfdController.h>
 #include <EqualsLikeCommandParser.h>
+#include <CommandAssembler.h>
 
 #define COMMANDBUFSIZE 50
+#define DEBOUNCE_TIME 100
+#define BAUD_RATE 9600
+#define ANIMATION_TIME_MS 100
+#define CYCLE_TIME_MS 2
 
 const int dataPin = 2;
 const int latchPin = 3;
@@ -28,21 +33,26 @@ LarsonScanner* show2;
 LarsonLightShowBuilder show2builder;
 Bounce debouncer;
 VfdController controller;
+CommandAssembler assembler;
 
 boolean pinState13=false;
 unsigned long lasttime1;
 unsigned long lasttime2;
 boolean lightShowState=false;
+char buffer0[COMMANDBUFSIZE];
+char buffer1[COMMANDBUFSIZE];
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(BAUD_RATE);
+  assembler.setBuffer(buffer0, COMMANDBUFSIZE);
+
   manipulator.setMirroring(0x01,20);
   shifter.begin(dataPin,latchPin,clockPin,&manipulator);
   pinMode(13,OUTPUT);
   pinMode(testPin, INPUT_PULLUP);
   debouncer.attach(testPin);
-  debouncer.interval(100);
+  debouncer.interval(DEBOUNCE_TIME);
 
   show1.addDigit(&(anotherVFD.digit1), 1);
   show1.addDigit(&(anotherVFD.digit2), 2);
@@ -62,6 +72,7 @@ void setup() {
 
 void loop(){
   debouncer.update();
+  assembler.addChar(Serial.read());
 
   if (debouncer.fell()) {
     lightShowState = !lightShowState;
@@ -70,15 +81,12 @@ void loop(){
     else anotherVFD._tdvd.setEnabled(false);
   }
  
-  if (Serial.available()>0) {
-    char command[COMMANDBUFSIZE];
-    strncpy(command,Serial.readString().c_str(),COMMANDBUFSIZE);
-    command[COMMANDBUFSIZE-1] = '\0';
-
-    controller.obey(command, &anotherVFD);
+  if (assembler.isCommandAvailable()) {
+    assembler.getCommand(buffer1,COMMANDBUFSIZE);
+    controller.obey(buffer1, &anotherVFD);
   }
   
-  if (millis() - lasttime2 > 100) {
+  if (millis() - lasttime2 > ANIMATION_TIME_MS) {
   	anotherVFD.animate();
     	digitalWrite(13,pinState13);
     	pinState13=~pinState13;
@@ -87,7 +95,7 @@ void loop(){
   	lasttime2 = millis();
   }
   
-  if (millis()-lasttime1 > 2) {
+  if (millis()-lasttime1 > CYCLE_TIME_MS) {
   	plexi.cycle();
 	lasttime1 = millis();
   }
