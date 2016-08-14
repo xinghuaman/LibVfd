79beacea8c6d381ee56a9ae9605d3479af004821
Kevin Flynn <666c796e6e@users.noreply.github.com>
2016-08-14 22:49:28 +0200

    Refactored it to not define all strings globally

--- VfdController.cpp
+++ VfdController.cpp
@@ -4,17 +4,18 @@
 #include <EqualsLikeCommandParser.h>
 
 #include <string.h>
+#include <avr/pgmspace.h>
 
 char VfdController::buffer[GENERICBUFSIZE];
 
 void VfdController::nocomprende(const char * const command,const char * const cause, AbstractVFD* vfd) {
-  Serial.print(F("Hein? "));
+  Serial.print(F("What? "));
   Serial.print(command);
-  Serial.print(F(" "));
+  Serial.print(F(" -->"));
   strncpy_P(buffer, cause, GENERICBUFSIZE);
   buffer[GENERICBUFSIZE-1] = '\0';
-  Serial.println(cause);
-  Serial.println(F("Valid commands:"));
+  Serial.print(buffer);
+  Serial.println(F("<--\nValid commands:"));
   AnimatableFunction** funcs = vfd->getFunctions();
   for(int i=0;i<vfd->getNumFunctions();i++) {
   	funcs[i]->getMemonic(buffer, GENERICBUFSIZE);
@@ -36,30 +37,30 @@ void VfdController::obey(char command[], AbstractVFD* vfd){
     EqualsLikeToken token;
 
     if (parser.parse(command, &token)!=0) {
-      nocomprende(command, global_noparse, vfd);
+      nocomprende(command, PSTR("Does not parse!"), vfd);
       return;
     }
 
     AnimatableFunction* func = vfd->getFunctionFor(token.subject);
     if (func == NULL) {
-      nocomprende(command, global_unkfunc, vfd);
+      nocomprende(command, PSTR("Unknown Function"), vfd);
       return;
     }
 
-    if (strcmp_P(token.object, PSTR("on")))
+    if (strcmp_P(token.object, PSTR("on"))==0)
        func->setEnabled(true);
-    else if (strcmp_P(token.object, PSTR("off")))
+    else if (strcmp_P(token.object, PSTR("off"))==0)
        func->setEnabled(false);
-    else if (strcmp_P(token.object, PSTR("blink")))
+    else if (strcmp_P(token.object, PSTR("blink"))==0)
        func->setBlink();
     else {
        func->getType(buffer, GENERICBUFSIZE);
-       if (strcmp_P(buffer, global_sevenseg) == 0 ) {
+       if (strcmp_P(buffer, PSTR("SevenSegmentEncoder")) == 0 ) {
        	 long number = strtol(token.object,NULL,10);
          SevenSegmentEncoder* en = (SevenSegmentEncoder*) func;          
          en->encode(number);
        } else {
-         nocomprende(command, global_nodig, vfd);
+         nocomprende(command, PSTR("That is no digit!"), vfd);
        }
     } 
 }
--- VfdController.h
+++ VfdController.h
@@ -5,12 +5,6 @@
 
 #define GENERICBUFSIZE 20
 
-const char global_noparse[] PROGMEM  = "Does not parse!";
-const char global_unkfunc[] PROGMEM  = "Unknown Function!";
-const char global_sevenseg[] PROGMEM = "SevenSegmentEncoder";
-const char global_nodig[] PROGMEM    = "It's not a digit!";
-const char global_unknerr[] PROGMEM  = "Unknown Error!";
-
 class VfdController {
 	private:
 		void nocomprende(const char* const command, const char* const cause, 
