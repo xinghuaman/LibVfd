344254744d3bf62df8ed4a795d534ed599bafe6d
Kevin Flynn <666c796e6e@users.noreply.github.com>
2016-08-14 21:35:50 +0200

    Pulled out RedGreenVcrVfd

---
+++ examples/RedGreenVfd/RedGreenVcrVfd.cpp
@@ -0,0 +1,193 @@
+#include <RedGreenVcrVfd.h>
+
+RedGreenVcrVfd::RedGreenVcrVfd()
+{
+  _bins[0] = 0x02; //Power, firstdigit_red, Playsympols
+  _bins[1] = 0x20;
+  _bins[2] = 0x400;
+  _bins[3] = 0x8000;
+  _bins[4] = 0x10000;//100000
+  _bins[5] = 0x40000;//1 00 00 00
+  _bins[6] = 0x100000;//1 00 00 00
+  _bins[7] = 0x800000; 
+  _bins[8] = 0x1000000;
+  _bins[9] = 0x2000000;
+ _numbins=8;
+
+   digit1.begin(10,RGV_digit1);
+   digit1.addSegment(&_mostused, _bins+1);
+   digit1.addSegment(&_mostused, _bins+3);
+   addFunc(&digit1);
+
+  //_mostused.begin(t, tl, tr, m, bl, br, b   , dp);
+  _mostused.begin(0x1000, 0x200, 0x100, 0x80, 0x10, 0x04, 0x01, 0x00);
+  // Almost all digits except
+  _outsider.begin( 0x00, 0x8000000, 0x4000000, 0x00, 0x20000, 0x4000, 0x2000   , 0x00); 
+  // Minute last digit seconts all digit
+
+  _pwr.begin(_bins+0,0x00,RGV_pwr); addFunc(&_pwr);
+
+  _sek.begin(_bins+0,0x00,RGV_sek); addFunc(&_sek);
+
+  //_l.begin(bin,mask,memonic);
+
+  _l.begin(_bins+1,0x01,RGV_l); addFunc(&_l);
+  _m.begin(_bins+5,0x01,RGV_m); addFunc(&_m);
+  _off.begin(_bins+2,0x01,RGV_off); addFunc(&_off);
+
+  _su.begin(_bins+3,0x04,RGV_su); addFunc(&_su);
+  _on.begin(_bins+2,0x04,RGV_on); addFunc(&_on);
+  _dp.begin(_bins+0,0x04,RGV_dp); addFunc(&_dp);
+  _s.begin(_bins+0,0x04,RGV_s); addFunc(&_s); 
+ 
+  // 0x08 shorted by the tube
+  
+  _p.begin(_bins+1,0x10,RGV_p); addFunc(&_p);
+  _am.begin(_bins+0,0x10,RGV_am); addFunc(&_am);
+  _3.begin(_bins+0,0x10,RGV_3); addFunc(&_3);
+
+  // 0x20 is a grid
+  // 0x40 is shorted by the tube
+
+  _5.begin(_bins+5,0x80,RGV_5); addFunc(&_5);
+  _ff.begin(_bins+5,0x80,RGV_ff); addFunc(&_ff);
+  _dual.begin(_bins+5,0x80,RGV_dual); addFunc(&_dual);
+
+  _wkly.begin(_bins+2,0x100,RGV_wkly); addFunc(&_wkly);
+  _rep.begin(_bins+2,0x100,RGV_rep); addFunc(&_rep);
+  _we.begin(_bins+2,0x100,RGV_we); addFunc(&_we);
+
+  _index.begin(_bins+1,0x200,RGV_index); addFunc(&_index);
+  _4.begin(_bins+5,0x200,RGV_4); addFunc(&_4);
+  _pm.begin(_bins+2,0x200,RGV_pm); addFunc(&_pm);
+
+  // 0x400 is a grid
+  // 0x800 is shorted by the tube
+  
+  _cas.begin(_bins+0,0x1000,RGV_cas); addFunc(&_cas);
+  _otr.begin(_bins+2,0x1000,RGV_otr); addFunc(&_otr);
+  _tu.begin(_bins+4,0x1000,RGV_tu); addFunc(&_tu);
+
+  _sek.begin(_bins+0,0x2000,RGV_sek); addFunc(&_sek);
+  _ch.begin(_bins+0,0x2000,RGV_ch); addFunc(&_ch);
+  _ply.begin(_bins+0,0x2000,RGV_ply); addFunc(&_ply);
+  _err.begin(_bins+0,0x2000,RGV_err); addFunc(&_err);
+
+  //Overflow is 4000,
+  _line.begin(_bins+0,0x4000,RGV_line); addFunc(&_line);
+  _hifi.begin(_bins+0,0x4000,RGV_hifi); addFunc(&_hifi);
+  //Additional symbols for pause like on 4000 too.
+  _h.begin(_bins+0,0x4000,RGV_h); addFunc(&_h);
+  _sa.begin(_bins+0,0x4000,RGV_sa); addFunc(&_sa);
+
+  // 0x  80 00 is a grid
+  // 0x1 00 00 is a grid
+
+
+
+  _rec.begin(_bins+1,0x20000,RGV_rec); addFunc(&_rec);
+  _catv.begin(_bins+1,0x20000,RGV_catv); addFunc(&_catv);
+  //Lowerdash is 20000
+  _1.begin(_bins+1,0x20000,RGV_1); addFunc(&_1);
+  _6.begin(_bins+1,0x20000,RGV_6); addFunc(&_6);
+
+// 0x 4 00 00 is a grid
+// 0x 8 00 00 is not connected by the tube
+// 0x10 00 00 is a grid
+// 0x20 00 00 is shorted by the tube
+
+  _vps.begin(_bins+1,0x400000,RGV_vps); addFunc(&_vps);
+  _8.begin(_bins+1,0x400000,RGV_8); addFunc(&_8);
+  _bs.begin(_bins+0,0x400000,RGV_bs); addFunc(&_bs);
+  // Two additional Points in Play are also on 400000
+
+// 0x  80 00 00 is a gate
+// 0x1 00 00 00 is a gate
+// 0x2 00 00 00 is a gate
+
+
+
+  _mo.begin(_bins+1,0x4000000,RGV_mo); addFunc(&_mo);
+  _clk.begin(_bins+1,0x4000000,RGV_clk); addFunc(&_clk);
+  _fr.begin(_bins+1,0x4000000,RGV_fr); addFunc(&_fr);
+  _sap.begin(_bins+1,0x4000000,RGV_sap); addFunc(&_sap);
+  _pse.begin(_bins+1,0x4000000,RGV_pse); addFunc(&_pse);
+  // upper minus is at 0x4 00 00 00
+
+
+   _fply.begin(_bins+1,0x8000000,RGV_fply); addFunc(&_fply);
+   _aft.begin(_bins+1,0x8000000,RGV_aft); addFunc(&_aft);
+   _dew.begin(_bins+1,0x8000000,RGV_dew); addFunc(&_dew);
+   _2.begin(_bins+1,0x8000000,RGV_2); addFunc(&_2);
+   _7.begin(_bins+1,0x8000000,RGV_7); addFunc(&_7);
+
+
+
+
+
+
+ /* 
+  _leftseg.begin(0x10, 0x40, 0x20, 0x80, 0x200, 0x100, 0x400, 0x00);
+  _rightseg.begin(0x1000, 0x4000, 0x2000, 0x8000, 0x20000, 0x10000, 0x40000, 0x00);
+
+  digit1.begin(10,"digit1");
+  digit1.addSegment(&_rightseg,_bins+0);
+  digit1.addSegment(&_rightseg,_bins+1);
+  addFunc(&digit1);
+
+  digit2.begin(10,"digit2");
+  digit2.addSegment(&_leftseg,_bins+2);
+  digit2.addSegment(&_rightseg,_bins+2);
+  addFunc(&digit2);
+
+  digit3.begin(10,"digit3");
+  digit3.addSegment(&_leftseg,_bins+3);
+  digit3.addSegment(&_rightseg,_bins+3);
+  addFunc(&digit3);
+
+  _tdvd.begin("ani",0x3F0);
+  _tdvd.addStep(0x10,_bins+0);
+  _tdvd.addStep(0x20,_bins+0);
+  _tdvd.addStep(0x40,_bins+0);
+  _tdvd.addStep(0x80,_bins+0);
+  _tdvd.addStep(0x100,_bins+0);
+  _tdvd.addStep(0x200,_bins+0);
+  addFunc(&_tdvd);
+
+  _dvdcenter.begin(_bins+0,0x400,"dvdcenter");
+  addFunc(&_dvdcenter);
+  _threezero.begin(_bins+0,0x800,"threezero");
+  addFunc(&_threezero);
+  _backslash.begin(_bins+0,0x8000,"backslash");
+  addFunc(&_backslash);
+
+  _v.begin(_bins+1,0x10,"v");
+  addFunc(&_v);
+  _cd.begin(_bins+1,0x20,"cd");
+  addFunc(&_cd);
+  _p.begin(_bins+1,0x40,"p");
+  addFunc(&_p);
+  _commas.begin(_bins+1,0x80,"commas");
+  addFunc(&_commas);
+  _dash.begin(_bins+1,0x100,"dash");
+  addFunc(&_dash);
+  _speakerl.begin(_bins+1,0x200,"spl");
+  addFunc(&_speakerl);
+  _speakerr.begin(_bins+1,0x400,"spr");
+  addFunc(&_speakerr);
+  _s.begin(_bins+1,0x800,"s");
+  addFunc(&_s);
+  _N.begin(_bins+1,0x80000,"N");
+  addFunc(&_N);
+  
+  _pbc.begin(_bins+2,0x800,"pbc");
+  addFunc(&_pbc);
+  _dvd.begin(_bins+2,0x80000,"dvd");
+  addFunc(&_dvd);
+
+  _mp3.begin(_bins+3,0x800,"mp3");
+  addFunc(&_mp3);
+  _doublepoint.begin(_bins+3,0x80000,"dp");
+  addFunc(&_doublepoint);
+*/
+};
---
+++ examples/RedGreenVfd/RedGreenVcrVfd.h
@@ -0,0 +1,149 @@
+#ifndef RedGreenVcrVfd_h
+#define RedGreenVcrVfd_h
+
+#include "AbstractVFD.h"
+#include "SevenSegments.h"
+#include "Animation.h"
+#include "SingleFunction.h"
+#include "SevenSegmentEncoder.h"
+
+const char RGV_digit1[] PROGMEM = "digit1";
+const char RGV_digit2[] PROGMEM = "digit2";
+const char RGV_digit3[] PROGMEM = "digit3";
+const char RGV_digit4[] PROGMEM = "digit4";
+const char RGV_digit5[] PROGMEM = "digit5";
+const char RGV_digit6[] PROGMEM = "digit6";
+
+const char RGV_dp[] PROGMEM = ":";
+
+const char RGV_pwr[] PROGMEM = "pwr";
+const char RGV_cas[] PROGMEM = "cas";
+const char RGV_vcr[] PROGMEM = "vcr";
+const char RGV_bs[] PROGMEM = "bs";
+const char RGV_clk[] PROGMEM = "clk";
+const char RGV_otr[] PROGMEM = "otr";
+const char RGV_wkly[] PROGMEM = "wkly";
+const char RGV_su[] PROGMEM = "su";
+const char RGV_mo[] PROGMEM = "mo";
+const char RGV_tu[] PROGMEM = "tu";
+const char RGV_we[] PROGMEM = "we";
+const char RGV_th[] PROGMEM = "th";
+const char RGV_fr[] PROGMEM = "fr";
+const char RGV_sa[] PROGMEM = "sa";
+const char RGV_line[] PROGMEM = "line";
+
+const char RGV_dual[] PROGMEM = "dual";
+const char RGV_on[] PROGMEM = "on";
+const char RGV_ch[] PROGMEM = "ch";
+const char RGV_st[] PROGMEM = "st";
+const char RGV_off[] PROGMEM = "off";
+const char RGV_catv[] PROGMEM = "catv";
+const char RGV_sap[] PROGMEM = "sap";
+const char RGV_am[] PROGMEM = "am";
+const char RGV_aft[] PROGMEM = "aft";
+const char RGV_hifi[] PROGMEM = "hifi";
+const char RGV_pm[] PROGMEM = "pm";
+const char RGV_err[] PROGMEM = "err";
+const char RGV_ply[] PROGMEM = "ply";
+const char RGV_fply[] PROGMEM = "fply";
+const char RGV_rec[] PROGMEM = "rec";
+const char RGV_pse[] PROGMEM = "pse";
+const char RGV_ff[] PROGMEM = "ff";
+const char RGV_rwd[] PROGMEM = "rwd";
+
+const char RGV_rep[] PROGMEM = "rep";
+const char RGV_s[] PROGMEM = "s";
+const char RGV_l[] PROGMEM = "l";
+const char RGV_p[] PROGMEM = "p";
+const char RGV_index[] PROGMEM = "index";
+const char RGV_dew[] PROGMEM = "dew";
+const char RGV_vps[] PROGMEM = "vps";
+const char RGV_pgm[] PROGMEM = "pgm";
+const char RGV_1[] PROGMEM = "1";
+const char RGV_2[] PROGMEM = "2";
+const char RGV_3[] PROGMEM = "3";
+const char RGV_4[] PROGMEM = "4";
+const char RGV_5[] PROGMEM = "5";
+const char RGV_6[] PROGMEM = "6";
+const char RGV_7[] PROGMEM = "7";
+const char RGV_8[] PROGMEM = "8";
+
+const char RGV_m[] PROGMEM = "m";
+const char RGV_h[] PROGMEM = "h";
+const char RGV_sek[] PROGMEM = "sek";
+
+
+class RedGreenVcrVfd : public AbstractVFD {
+  public:
+    SevenSegments _mostused;
+    SevenSegments _outsider;
+   
+    SevenSegmentEncoder digit1;
+    SevenSegmentEncoder digit2;
+    SevenSegmentEncoder digit3;
+    SevenSegmentEncoder digit4;
+    SevenSegmentEncoder digit5;
+    SevenSegmentEncoder digit6;
+
+    SingleFunction _dp;
+
+    SingleFunction _pwr;
+    SingleFunction _cas;
+    SingleFunction _vcr;
+    SingleFunction _bs;
+    SingleFunction _clk;
+    SingleFunction _otr;
+    SingleFunction _wkly;
+    SingleFunction _su;
+    SingleFunction _mo;
+    SingleFunction _tu;
+    SingleFunction _we;
+    SingleFunction _th;
+    SingleFunction _fr;
+    SingleFunction _sa;
+
+    SingleFunction _line;
+    SingleFunction _dual;
+    SingleFunction _on;
+    SingleFunction _ch;
+    SingleFunction _st;
+    SingleFunction _off;
+    SingleFunction _catv;
+    SingleFunction _sap;
+    SingleFunction _am;
+    SingleFunction _aft;
+    SingleFunction _hifi;
+    SingleFunction _pm;
+    SingleFunction _err;
+
+    SingleFunction _ply;
+    SingleFunction _fply;
+    SingleFunction _rec;
+    SingleFunction _pse;
+    SingleFunction _ff;
+    SingleFunction _rwd;
+
+    SingleFunction _rep;
+    SingleFunction _s;
+    SingleFunction _l;
+    SingleFunction _p;
+    SingleFunction _index;
+    SingleFunction _dew;
+    SingleFunction _vps;
+    SingleFunction _pgm;
+    SingleFunction _1;
+    SingleFunction _2;
+    SingleFunction _3;
+    SingleFunction _4;
+    SingleFunction _5;
+    SingleFunction _6;
+    SingleFunction _7;
+    SingleFunction _8;
+
+    SingleFunction _m;
+    SingleFunction _h;
+    SingleFunction _sek;
+    RedGreenVcrVfd();
+};
+
+#endif
