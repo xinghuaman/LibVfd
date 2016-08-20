5e8668c4a1f32a4db9a760c8da4c8b8dcfee099b
Kevin Flynn <666c796e6e@users.noreply.github.com>
2016-08-20 08:49:41 +0200

    Completed VcrVfd

--- examples/RedGreenVfdController/RedGreenVcrVfd.cpp
+++ examples/RedGreenVfdController/RedGreenVcrVfd.cpp
@@ -53,14 +53,12 @@ RedGreenVcrVfd::RedGreenVcrVfd()
 
 
   _l.begin(_bins+1,0x01,RGV_l); addFunc(&_l);
-  
-  _m.begin(_bins+7,0x01,RGV_m); addFunc(&_m);
   _off.begin(_bins+3,0x01,RGV_off); addFunc(&_off);
+  _m.begin(_bins+7,0x01,RGV_m); addFunc(&_m);
 
-  _su.begin(_bins+5,0x04,RGV_su); addFunc(&_su);// Does not work!
+  _s.begin(_bins+1,0x04,RGV_s); addFunc(&_s); 
   _on.begin(_bins+3,0x04,RGV_on); addFunc(&_on);
   _dp.begin(_bins+7,0x04,RGV_dp); addFunc(&_dp);
-  _s.begin(_bins+1,0x04,RGV_s); addFunc(&_s); 
  
   // 0x08 shorted by the tube
   
@@ -71,12 +69,12 @@ RedGreenVcrVfd::RedGreenVcrVfd()
   // 0x20 is a grid
   // 0x40 is shorted by the tube
 
-  _5.begin(_bins+7,0x80,RGV_5); addFunc(&_5);
   _ff.begin(_bins+1,0x80,RGV_ff); addFunc(&_ff);
   _dual.begin(_bins+3,0x80,RGV_dual); addFunc(&_dual);
+  _5.begin(_bins+7,0x80,RGV_5); addFunc(&_5);
 
-  _wkly.begin(_bins+3,0x100,RGV_wkly); addFunc(&_wkly);
   _rep.begin(_bins+1,0x100,RGV_rep); addFunc(&_rep);
+  _wkly.begin(_bins+3,0x100,RGV_wkly); addFunc(&_wkly);
   _we.begin(_bins+7,0x100,RGV_we); addFunc(&_we);
 
   _index.begin(_bins+1,0x200,RGV_index); addFunc(&_index);
@@ -90,27 +88,25 @@ RedGreenVcrVfd::RedGreenVcrVfd()
   _otr.begin(_bins+3,0x1000,RGV_otr); addFunc(&_otr);
   _tu.begin(_bins+7,0x1000,RGV_tu); addFunc(&_tu);
 
-   _ch.begin(_bins+2,0x2000,RGV_ch); addFunc(&_ch);
   _ply.begin(_bins+0,0x2000,RGV_ply); addFunc(&_ply);
+   _ch.begin(_bins+2,0x2000,RGV_ch); addFunc(&_ch);
   _err.begin(_bins+3,0x2000,RGV_err); addFunc(&_err);
   _sek.begin(_bins+9,0x2000,RGV_sek); addFunc(&_sek);
 
-  //Overflow is 4000,
+  _ovrflw1.begin(_bins+0,0x4000,RGV_ovrflw1); addFunc(&_ovrflw1);
+  _ppse.begin(_bins+1,0x4000,RGV_ppse); addFunc(&_ppse);
   _line.begin(_bins+2,0x4000,RGV_line); addFunc(&_line);
   _hifi.begin(_bins+3,0x4000,RGV_hifi); addFunc(&_hifi);
-  //Additional symbols for pause like on 4000 too.
-  //_h.begin(_bins+0,0x4000,RGV_h); addFunc(&_h);
+  _h.begin(_bins+4,0x4000,RGV_h); addFunc(&_h);
   _sa.begin(_bins+9,0x4000,RGV_sa); addFunc(&_sa);
 
   // 0x  80 00 is a grid
   // 0x1 00 00 is a grid
 
-
-
   _rec.begin(_bins+0,0x20000,RGV_rec); addFunc(&_rec);
   _catv.begin(_bins+2,0x20000,RGV_catv); addFunc(&_catv);
-  //Lowerdash is 20000
-  //_1.begin(_bins+1,0x20000,RGV_1); addFunc(&_1); // does not work
+  _ldash.begin(_bins+3,0x20000,RGV_ldash); addFunc(&_ldash);
+  _1.begin(_bins+5,0x20000,RGV_1); addFunc(&_1);
   _6.begin(_bins+9,0x20000,RGV_6); addFunc(&_6);
 
 // 0x 4 00 00 is a grid
@@ -120,9 +116,11 @@ RedGreenVcrVfd::RedGreenVcrVfd()
 
   _vps.begin(_bins+3,0x400000,RGV_vps); addFunc(&_vps);
   _8.begin(_bins+9,0x400000,RGV_8); addFunc(&_8);
-  //_bs.begin(_bins+0,0x400000,RGV_bs); addFunc(&_bs);
+  _bs.begin(_bins+0,0x400000,RGV_bs); addFunc(&_bs);
+
   _pply.begin(_bins+0,0x400000,RGV_pply); addFunc(&_pply);
 
+  _bn(_bins+0,0x8000000,RGV_fply); addFunc(&_fply);
 // 0x  80 00 00 is a gate
 // 0x1 00 00 00 is a gate
 // 0x2 00 00 00 is a gate
@@ -133,25 +131,23 @@ RedGreenVcrVfd::RedGreenVcrVfd()
   _clk.begin(_bins+2,0x4000000,RGV_clk); addFunc(&_clk);
   _fr.begin(_bins+9,0x4000000,RGV_fr); addFunc(&_fr);
   _sap.begin(_bins+3,0x4000000,RGV_sap); addFunc(&_sap);
-  //_pse.begin(_bins+1,0x4000000,RGV_pse); addFunc(&_pse);
+  _pse.begin(_bins+1,0x4000000,RGV_pse); addFunc(&_pse);
   // upper minus is at 0x4 00 00 00
 
-  // lower minus is on grid 3
 
 
    _fply.begin(_bins+0,0x8000000,RGV_fply); addFunc(&_fply);
    _aft.begin(_bins+2,0x8000000,RGV_aft); addFunc(&_aft);
    _dew.begin(_bins+3,0x8000000,RGV_dew); addFunc(&_dew);
-   //_2.begin(_bins+1,0x8000000,RGV_2); addFunc(&_2); // Todo!
+   _2.begin(_bins+5,0x8000000,RGV_2); addFunc(&_2); 
    _7.begin(_bins+9,0x8000000,RGV_7); addFunc(&_7);
 
   _pwr.begin(_bins+0,0x10000000,RGV_pwr); addFunc(&_pwr);
   _vcr.begin(_bins+2,0x10000000,RGV_vcr); addFunc(&_vcr);
-  //_su.begin(_bins+0,0x10000000,RGV_su); addFunc(&_su);
   _th.begin(_bins+9,0x10000000,RGV_th); addFunc(&_th);
   _st.begin(_bins+3,0x10000000,RGV_st); addFunc(&_st);
   _rwd.begin(_bins+0,0x10000000,RGV_rwd); addFunc(&_rwd);
-  //_pwr.begin(_bins+0,0x10000000,RGV_pwr); addFunc(&_pwr);
+  _su.begin(_bins+5,0x10000000,RGV_su); addFunc(&_su);
 
   _pgm.begin(_bins+5,0x00,RGV_pgm); addFunc(&_pgm);
 };
